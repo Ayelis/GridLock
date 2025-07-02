c16927234a0c250792108bc3821dea5b7cfd1818
Steve <s.mattison@gmail.com>
2025-07-02 05:41:06 -0700

    Added sound effects for screen changes, 1-6 keys test pings

--- src/UI.gd
+++ src/UI.gd
@@ -2,10 +2,20 @@
 extends CanvasLayer
 
 var screens := []
+var audio_player := AudioStreamPlayer.new()
+
+const annoucements = {
+	1: "res://resources/audio/announcer/ready.ogg",
+	2: "res://resources/audio/announcer/final_round.ogg",
+	3: "res://resources/audio/announcer/you_win.ogg",
+}
 
 func _ready():
+	audio_player.bus = "Announcer"
+	add_child(audio_player)
+	#get_tree().root.add_child(audio_player)
 	screens = [ $MainMenu, $Lobby, $GameUI, $Results ]
-
+	
 	# Connect each TextureButton's pressed signal
 	for i in range(screens.size()):
 		var screen = screens[i]
@@ -13,6 +23,12 @@ func _ready():
 		if button:
 			button.pressed.connect(_on_screen_button_pressed.bind(i))
 	show_screen(0)
+
+# Call this to play any sound
+func play_audio(sound_path: String) -> void:
+	#audio_player.connect("finished", audio_player.queue_free)
+	audio_player.stream = load(sound_path)  # Load the sound dynamically
+	audio_player.play()
 
 func _on_screen_button_pressed(current_index: int) -> void:
 	var next_index = (current_index + 1) % screens.size()
@@ -23,6 +39,8 @@ func _on_screen_button_pressed(current_index: int) -> void:
 	else:
 		$"../Camera2D".visible = false
 		$"../World".visible = false
+	if(next_index!=0):
+		play_audio(annoucements[next_index])
 
 func show_screen(index: int) -> void:
 	for i in range(screens.size()):
--- src/autoloads/InputManager.gd
+++ src/autoloads/InputManager.gd
@@ -3,6 +3,20 @@ extends Node
 
 @export var joystick_vector := Vector2.ZERO  # Set by mobile UI if used
 
+const PING_PATHS = {
+	"ping_1": "res://resources/audio/pings/rally_help.ogg",
+	"ping_2": "res://resources/audio/pings/attack_info.ogg",
+	"ping_3": "res://resources/audio/pings/danger_warded.ogg",
+	"ping_4": "res://resources/audio/pings/defend_retreat.ogg",
+	"ping_5": "res://resources/audio/pings/enemymissing.ogg",
+	"ping_6": "res://resources/audio/pings/omw_ultsoon.ogg",
+}
+var PINGS = {}
+
+func _ready():
+	for action in PING_PATHS.keys():
+		PINGS[action] = load(PING_PATHS[action])  # preload() not allowed dynamically
+
 func get_move_input() -> Vector2:
 	var input_vector = Vector2.ZERO
 
@@ -15,3 +29,16 @@ func get_move_input() -> Vector2:
 		input_vector = joystick_vector
 
 	return input_vector.normalized()
+
+func _unhandled_input(event):
+	get_ping_input(event)
+
+func get_ping_input(event) -> void:
+	for action in PINGS.keys():
+		if Input.is_action_just_pressed(action):
+			var player = AudioStreamPlayer.new()
+			player.stream = PINGS[action]
+			player.bus = "Pings"
+			get_tree().root.add_child(player)
+			player.play()
+			player.connect("finished", player.queue_free)
